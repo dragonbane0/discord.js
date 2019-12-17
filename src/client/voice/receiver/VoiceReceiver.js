96875e39c50fe2785a2540c91a1d5528dbb7fbe4
Marwin Misselhorn <marwinmisselhorn@hotmail.de>
2019-12-17 21:15:15 +0100

    Tweaked voice delay again slightly

--- src/client/voice/receiver/VoiceReceiver.js
+++ src/client/voice/receiver/VoiceReceiver.js
@@ -104,7 +104,7 @@ class VoiceReceiver extends EventEmitter {
             // Connection already closed, ignore
             console.log("Connection already closed, skip onSpeaking event!");
           }
-        }, 70); //70 ms is solid. Discord client recommends 250 ms
+        }, 80); //80 ms is solid. Discord client recommends 250 ms
 
         this.speakingTimeouts.set(ssrc, speakingTimeout);
       } else {
